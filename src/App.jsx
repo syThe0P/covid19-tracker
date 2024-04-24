237615fed063788795b2277536950f7a7cb3f4f3
Pranav Kumar <pranavkumar1522@gmail.com>
2024-04-24 21:14:57 +0530

    Setup a project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="app_header">
+        <h1>COVID-19 TRACKER</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
